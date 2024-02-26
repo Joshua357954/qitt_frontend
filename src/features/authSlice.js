700d2be4d5096924f335ea7db223c9a22ac53d25
joshua boyi <boyijoshua72@gmail.com>
2024-02-26 16:22:14 +0100

    new

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -1,3 +1,4 @@
+
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { baseUrl } from '../utils/utils.js';
 import axios from 'axios';
@@ -5,15 +6,22 @@ import axios from 'axios';
 const apiUrl = `${baseUrl}/api/auth`;
 
 // Create async thunk for user registration
-export const registerUser = createAsyncThunk('user/registerUser', async (userData) => {
-  const response = await axios.post(`${apiUrl}/register`, userData);
-  return response.data;
+export const registerUser = createAsyncThunk('user/registerUser', async (userData, { rejectWithValue }) => {
+  try {
+    const response = await axios.post(`${apiUrl}/register`, userData);
+    return response.data;
+  } catch (error) {
+    return rejectWithValue(error.response.data.message);
+  }
 });
 
-export const loginUser = createAsyncThunk('user/loginUser', async (credentials) => {
-  const response = await axios.post(`${apiUrl}/login`, credentials);
-  console.log(response)
-  return response.data;
+export const loginUser = createAsyncThunk('user/loginUser', async (credentials, { rejectWithValue }) => {
+  try {
+    const response = await axios.post(`${apiUrl}/login`, credentials);
+    return response.data;
+  } catch (error) {
+    return rejectWithValue(error.response.data.message);
+  }
 });
 
 // Create user slice
@@ -28,7 +36,7 @@ const userSlice = createSlice({
     faculty: '',
     department: '',
     session: '',
-    imageUrl:'',
+    imageUrl: '',
     courseName: '',
     status: 'idle', // Possible values: 'idle', 'loading', 'succeeded', 'failed'
     error: null,
@@ -39,47 +47,55 @@ const userSlice = createSlice({
     builder
       .addCase(registerUser.pending, (state) => {
         state.status = 'loading';
+        state.error = null; // Clear previous errors
       })
       .addCase(registerUser.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        // Update state with registered user data
-        state.name = action.payload.name;
-        state.email = action.payload.email;
-        state.joined = action.payload.createdAt;
-        state.gender = action.payload.gender;
-        state.dateOfBirth = action.payload.dateOfBirth;
-        state.faculty = action.payload.faculty;
-        state.department = action.payload.department;
-        state.session = action.payload.session;
-        state.imageUrl = action.payload.imageUrl;
-        state.message = action.payload.message;
-        state.courseName = action.payload.courseName
+        return {
+          ...state,
+          status: 'succeeded',
+          // Update state with registered user data
+          name: action.payload.name,
+          email: action.payload.email,
+          joined: action.payload.createdAt,
+          gender: action.payload.gender,
+          dateOfBirth: action.payload.dateOfBirth,
+          faculty: action.payload.faculty,
+          department: action.payload.department,
+          session: action.payload.session,
+          imageUrl: action.payload.imageUrl,
+          message: action.payload.message,
+          courseName: action.payload.courseName,
+        };
       })
       .addCase(registerUser.rejected, (state, action) => {
         state.status = 'failed';
-        state.error = action.error.message;
+        state.error = action.payload;
       })
       .addCase(loginUser.pending, (state) => {
         state.status = 'loading';
+        state.error = null; // Clear previous errors
       })
       .addCase(loginUser.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        // Assuming action.payload has the expected fields
-        state.name = action.payload.name;
-        state.email = action.payload.email;
-        state.joined = action.payload.createdAt;
-        state.gender = action.payload.gender;
-        state.dateOfBirth = action.payload.dateOfBirth;
-        state.faculty = action.payload.faculty;
-        state.department = action.payload.department;
-        state.session = action.payload.session;
-        state.imageUrl = action.payload.imageUrl;
-        state.message = action.payload.message;
-        state.courseName = action.payload.courseName
+        return {
+          ...state,
+          status: 'succeeded',
+          // Assuming action.payload has the expected fields
+          name: action.payload.name,
+          email: action.payload.email,
+          joined: action.payload.createdAt,
+          gender: action.payload.gender,
+          dateOfBirth: action.payload.dateOfBirth,
+          faculty: action.payload.faculty,
+          department: action.payload.department,
+          session: action.payload.session,
+          imageUrl: action.payload.imageUrl,
+          message: action.payload.message,
+          courseName: action.payload.courseName,
+        };
       })
       .addCase(loginUser.rejected, (state, action) => {
         state.status = 'failed';
-        state.error = action.error.message;
+        state.error = action.payload;
       });
   },
 });
